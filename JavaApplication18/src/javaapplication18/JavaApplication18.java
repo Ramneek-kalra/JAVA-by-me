a7ced562a51301a6c6acd7910b34a09213100fc9
WadudAzad <azadmiu37@gmail.com>
2018-10-06 12:43:47 +0600

    add

---
+++ JavaApplication18/src/javaapplication18/JavaApplication18.java
@@ -0,0 +1,12 @@
+
+package javaapplication18;
+
+
+public class TestCollecton {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ RemovrDiak/src/removrdiak/RemovrDiak.java
@@ -0,0 +1,14 @@
+
+package removrdiak;
+
+public class RemovrDiak {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    public static void removrdiak(int n, chr fromTower, char toTower,char formTower){
+        char toTower
+    }
+            
+            
+}
