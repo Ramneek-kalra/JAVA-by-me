80ef1b4087a40355bea466196f37c694255491ab
Ramneek Kalra <kalraramneekhmr@gmail.com>
2018-10-14 12:50:48 +0530

    Improvements using Comments so that User can understand better!

--- chapter 1/HelloWorld.java
+++ chapter 1/HelloWorld.java
@@ -1,9 +1,17 @@
+//Welcome to Chapter 1 where you can find how to define "Hello World" Program
+
 public class HelloWorld{
-public static String msg;
-public static String name="Afrin";
-public static void main(String[] args){
-msg="Hello World!!";
-System.out.println("Message: "+msg);
-System.out.println("Name: "+name);
-}
+
+	//Initialising the Variables
+	public static String msg;
+	public static String name="Afrin";
+
+	//Defining Main Method
+	public static void main(String[] args){
+		msg="Hello World!!";
+
+		//Using Print Statement to print Message
+		System.out.println("Message: "+msg);
+		System.out.println("Name: "+name);
+	}
 }
--- chapter 2/Chapter 2/src/chapter/pkg2/ComputeAverage.java
+++ chapter 2/Chapter 2/src/chapter/pkg2/ComputeAverage.java
@@ -1,25 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package chapter.pkg2;
-
+//Defining required Package for I/O Functions
 import java.util.Scanner;
 
-/**
- *
- * @author User
- */
 public class ComputeAverage {
     public static void main (String[] args){
+    	//Defining Scanner Class for Inputting Data
         Scanner input = new Scanner(System.in);
         
+        //Scanning the Values from User
         System.out.print("Enter three numbers: ");
         double number1 = input.nextDouble();
         double number2 = input.nextDouble();
         double number3 = input.nextDouble();
         
+        //Computing the Average of Inputted Values
         double average = (number1 + number2 +number3)/3;
         
         System.out.println("The average of "+number1+", "+number2+", "+number3+" is "+ average);
